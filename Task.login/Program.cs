981dd97875fa55748ca9896bf00871059fe71b31
Alizadehh1 <alisal@code.edu.az>
2021-12-09 20:47:16 -0800

    salam

---
+++ Task.login/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task.login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+
+            Console.Write("User Name: ");
+            u.UserName = Console.ReadLine();
+
+            Console.Write("Password: ");
+            u.Password = Console.ReadLine();
+
+            bool IsLoggedOn = u.Login();
+
+            if (IsLoggedOn)
+            {
+                Console.WriteLine($"Xosh gelmisiz @{u.UserName}");
+            }
+            else
+            {
+                Console.WriteLine("Sansinizi  bir daha sinayin!");
+            }
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
---
+++ Task.login/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.login
+{
+    class User
+    {
+        string username;
+        string password;
+
+        public string UserName {
+            get
+            {
+                return username;
+
+            }
+            set
+            {
+                this.username = value;
+                if (value.Length<5)
+                {
+                    throw new Exception("Minimum 5 simvoldan ibaret olmalidir!");
+                    
+                }
+
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+               this.password = value;
+                if (value.Length<8)
+                {
+                    throw new Exception("Minimum 8 Simvoldan ibaret olmalidir!");
+                }
+            }
+
+        }
+
+
+        public bool Login()
+        {
+            if (username=="Alizadehh" && password=="987654321")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+        
+
+        
+
+        public User()
+        {
+            Console.WriteLine("Sisteme Xosh gelmishsiniz!");
+        }
+    }
+}
